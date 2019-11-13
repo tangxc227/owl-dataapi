6cb40526432d673bb8257dcadfd89c22d87d50b9
tangxc227 <943365505@qq.com>
2019-11-13 09:16:03 +0800

    init

---
+++ src/main/java/com/tangxc/dataapi/OwlDataapiApplication.java
@@ -0,0 +1,16 @@
+package com.tangxc.dataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Xicheng.Tang
+ */
+@SpringBootApplication
+public class OwlDataapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwlDataapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tangxc/dataapi/OwlDataapiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tangxc.dataapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author Xicheng.Tang
+ */
+@SpringBootTest
+class OwlDataapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
